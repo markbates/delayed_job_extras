6377d997eb859407e46292cab7f61dfef044a05e
Mark Bates <mark@markbates.com>
2009-08-05 15:05:05 -0400

    Added date range report.

--- lib/delayed_job_extras/job.rb
+++ lib/delayed_job_extras/job.rb
@@ -15,7 +15,7 @@ module Delayed
     
     class << self
       
-      def stats
+      def stats(start_date = 1.day.ago.beginning_of_day, end_date = Time.now.beginning_of_day)
         workers = {}
         Delayed::Job.find_with_destroyed(:all, :select => :worker_class_name, :group => :worker_class_name).each do |dj|
           dj.worker_class_name = 'UNKNOWN' if dj.worker_class_name.blank?
@@ -27,6 +27,14 @@ module Delayed
           stats[:remaining] = Delayed::Job.count(:conditions => {:worker_class_name => wname})
           stats[:processed] = stats[:total] - stats[:remaining]
           stats[:failures] = Delayed::Job.count(:conditions => ['worker_class_name = ? and attempts > 1', wname])
+          if start_date
+            date_stats = {:start_date => start_date, :end_date => end_date}
+            date_stats[:total] = Delayed::Job.count_with_destroyed(:conditions => ['worker_class_name = ? and (created_at > ? and created_at < ?)', wname, start_date, end_date])
+            date_stats[:remaining] = Delayed::Job.count(:conditions => ['worker_class_name = ? and (created_at > ? and created_at < ?)', wname, start_date, end_date])
+            date_stats[:processed] = stats[:total] - stats[:remaining]
+            date_stats[:failures] = Delayed::Job.count(:conditions => ['worker_class_name = ? and attempts > 1 and (created_at > ? and created_at < ?)', wname, start_date, end_date])
+            stats[:date_range] = date_stats
+          end
           workers[worker] = stats
         end
         workers
