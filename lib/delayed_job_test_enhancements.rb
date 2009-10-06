89a1808d172f55d2842c3fee072d05ec8f626f07
Mark Bates <mark@markbates.com>
2009-10-06 15:49:56 -0400

    Fixed issue with delayed_job_test_enhancements being required twice and invoking jobs more than once.

--- lib/delayed_job_test_enhancements.rb
+++ lib/delayed_job_test_enhancements.rb
@@ -1,14 +1,19 @@
-module Delayed
-  class Job
+unless defined?('DELAYED_JOB_TEST_ENHANCEMENTS')
+  
+  module Delayed
+    class Job
     
-    class << self
-      def enqueue_with_work_off(obj, *args)
-        job = enqueue_without_work_off(obj, *args)
-        job.invoke_job
-        return job
+      class << self
+        def enqueue_with_work_off(obj, *args)
+          job = enqueue_without_work_off(obj, *args)
+          job.invoke_job
+          return job
+        end
+        alias_method_chain :enqueue, :work_off
       end
-      alias_method_chain :enqueue, :work_off
-    end
     
-  end
-end
+    end # Job
+  end # Delayed
+  
+  DELAYED_JOB_TEST_ENHANCEMENTS = 1
+end # unless defined?('DELAYED_JOB_TEST_ENHANCEMENTS')
