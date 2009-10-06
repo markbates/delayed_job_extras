1b6e40701d8c31145083fdb209d467b3c6fab734
Mark Bates <mark@markbates.com>
2009-10-06 12:43:45 -0400

    changed work_off to invoke_job in testing.

--- lib/delayed_job_test_enhancements.rb
+++ lib/delayed_job_test_enhancements.rb
@@ -4,7 +4,7 @@ module Delayed
     class << self
       def enqueue_with_work_off(obj, *args)
         job = enqueue_without_work_off(obj, *args)
-        Delayed::Job.work_off
+        job.invoke_job
         return job
       end
       alias_method_chain :enqueue, :work_off
