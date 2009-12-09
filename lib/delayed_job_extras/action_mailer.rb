1dc7db15aeac5497db242afb9b88e75c0c0a33e0
Mark Bates <mark@markbates.com>
2009-12-09 14:07:56 -0500

    Fixed small issue with ActionMailer and method_missing

--- lib/delayed_job_extras/action_mailer.rb
+++ lib/delayed_job_extras/action_mailer.rb
@@ -2,51 +2,54 @@ if defined?(ActionMailer)
   module ActionMailer
     class Base
         
-      def self.inherited(klass)
-        super
-        eval %{
-          class ::#{klass}Worker < DJ::Worker
-
-            attr_accessor :called_method
-            attr_accessor :args
-
-            def initialize(called_method, *args)
-              self.called_method = called_method
-              self.args = args
-            end
-
-            def perform
-              # ::#{klass}.send(self.called_method, *self.args)
-              ::#{klass}.send(:new, self.called_method, *self.args).deliver!
-            end
-            
-            class << self
-              
-              def method_missing(sym, *args)
-                ::#{klass}Worker.enqueue(sym, *args)
-              end
-              
-            end
-
-          end
-        }
-      end
-      
       class << self
         
-        def method_missing(method_symbol, *parameters) #:nodoc:
+        def inherited(klass)
+          super
+          eval %{
+            class ::#{klass}Worker < DJ::Worker
+
+              attr_accessor :called_method
+              attr_accessor :args
+
+              def initialize(called_method, *args)
+                self.called_method = called_method
+                self.args = args
+              end
+
+              def perform
+                # ::#{klass}.send(self.called_method, *self.args)
+                ::#{klass}.send(:new, self.called_method, *self.args).deliver!
+              end
+
+              class << self
+
+                def method_missing(sym, *args)
+                  ::#{klass}Worker.enqueue(sym, *args)
+                end
+
+              end
+
+            end
+          }
+        end
+        
+        def method_missing_with_extras(method_symbol, *parameters) #:nodoc:
           if match = matches_dynamic_method?(method_symbol)
             case match[1]
               when 'deliver'# then new(match[2], *parameters).deliver!
                 "#{self.name}Worker".constantize.enqueue(match[2], *parameters)
-              else super
+              else
+                method_missing_without_extras(method_symbol, *parameters)
             end
           else
             super
           end
         end
         
-      end
+        alias_method_chain :method_missing, :extras
+        
+      end # class << self
       
     end # Base
   end # ActionMailer
