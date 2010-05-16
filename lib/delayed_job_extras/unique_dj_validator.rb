6c39f809e8e13fa874064211e13b1ef2c74e3d7c
Mark Bates <mark@markbates.com>
2010-05-16 15:10:16 -0400

    Fixed an issue with Rails 3

--- lib/delayed_job_extras/unique_dj_validator.rb
+++ lib/delayed_job_extras/unique_dj_validator.rb
@@ -1,10 +1,10 @@
 # Rails 3 style validation:
 class UniqueDJValidator < ActiveModel::Validator
-  def validate()
+  def validate(record)
     
     if record.payload_object.respond_to?(:unique?) && record.new_record?
       if record.payload_object.unique?
-        if Delayed::Job.count(:all, :conditions => {:worker_class_name => record.worker_class_name, :finished_at => nil}) > 0
+        if Delayed::Job.where(:worker_class_name => 'self.worker_class_name', :finished_at => nil).count > 0
           record.errors.add_to_base("Only one #{record.worker_class_name} can be queued at a time!")
         end
       end
